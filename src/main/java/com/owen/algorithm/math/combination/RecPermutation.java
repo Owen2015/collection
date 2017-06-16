13ea86c87156f2b9050a1b596a88d1ceda3655de
owen <owen.chen86@yahoo.com>
2017-06-16 17:12:07 +0800

    a working version of permutation of string

--- src/main/java/com/owen/algorithm/math/combination/RecPermutation.java
+++ src/main/java/com/owen/algorithm/math/combination/RecPermutation.java
@@ -1,20 +1,41 @@
 package com.owen.algorithm.math.combination;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class RecPermutation {
 
 	public static void permute(String soFar,String rest){
-		if(rest==""){
+		if(rest.isEmpty()){
+			System.out.println(soFar);
 			return;
 		}else{
-			String next=null;
+			//String next=null;
 			for(int i=0;i<rest.length();i++){
-				soFar=soFar+rest.charAt(i);
-				
-				System.out.println(soFar);
+				String next=soFar+rest.charAt(i);
 				String remain=rest.substring(0, i)+rest.substring(i+1, rest.length());
+				//System.out.println("next: "+next+", rest: "+remain);
 				permute(next,remain);
 			}
-
+		}
+	}
+	public static void permute(List<?> soFar,List<?> rest){
+		if(rest.isEmpty()){
+			
+		}else{
+			for(int i=0;i<rest.size();i++){
+				//soFar.add((Object)rest.get(i));
+				List<?> next =new ArrayList<Object>();
+			}
+		}
+	}
+	
+	public static void subset(String soFar,String rest){
+		if(rest.isEmpty()){
+			return;
+		}else{
+			
 		}
 	}
 
