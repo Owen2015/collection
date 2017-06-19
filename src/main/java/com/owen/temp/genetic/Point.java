338cb54b614b1c2eee9eb3a1b0281887dddd6d11
owen <owen.chen86@yahoo.com>
2017-06-19 17:21:00 +0800

    add tsp algorithm using genetic implementation

---
+++ src/main/java/com/owen/temp/genetic/Point.java
@@ -0,0 +1,21 @@
+package com.owen.temp.genetic;
+
+
+/**
+ * Point
+ *
+ * @author: onlylemi
+ */
+
+public class Point {
+    public float x;
+    public float y;
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
